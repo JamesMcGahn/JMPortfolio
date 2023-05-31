2c0b106e0ab0cea78b79884b875ec835e27855b2
James McGahn <jmcgahn@gmail.com>
2023-05-31 17:55:34 -0400

    update sitemap xml to use slug

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,13 +1,8 @@
-import React from 'react';
-import axios from 'axios'
+import React from "react";
+import axios from "axios";
 
 function SiteMap(props) {
-
-    return (
-        <div>
-
-        </div>
-    );
+  return <div></div>;
 }
 
 export default SiteMap;
@@ -15,24 +10,22 @@ export default SiteMap;
 const createSitemap = (projects) => `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
         ${projects
-        .map(({ _id }) => {
+          .map(({ slug }) => {
             return `
                 <url>
-                    <loc>${`https://www.jamesmcgahn.com/projects/${_id}`}</loc>
+                    <loc>${`https://www.jamesmcgahn.com/projects/${slug}`}</loc>
                 </url>
             `;
-        })
-        .join('')}
+          })
+          .join("")}
     </urlset>
     `;
 
 export async function getServerSideProps({ res }) {
-    const response = await axios.get(`${process.env.SERVER}/api/projects/`)
-    const { data } = await response.data
-    res.setHeader('Content-Type', 'text/xml');
-    res.write(createSitemap(data));
-    res.end();
-    return { props: {} }
+  const response = await axios.get(`${process.env.SERVER}/api/projects/`);
+  const { data } = await response.data;
+  res.setHeader("Content-Type", "text/xml");
+  res.write(createSitemap(data));
+  res.end();
+  return { props: {} };
 }
-
-
