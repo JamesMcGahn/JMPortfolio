b576201cedce287d082157691b8d391c917f9ece
James McGahn <jmcgahn@gmail.com>
2023-05-31 10:26:33 -0400

    use slug instead of mongoId
    - API get project[ID] query by slug
    - project ID page - update static paths and static props to use slug
    - update project section links to use slug link

--- pages/api/projects/[id].js
+++ pages/api/projects/[id].js
@@ -1,29 +1,27 @@
 import dbConnect from "../../../utils/dbConnect";
-import Project from "../../../models/Project.js"
-
-
+import Project from "../../../models/Project.js";
 
 export default async function getProjectId(req, res) {
-    const {
-        query: { id },
-        method
-    } = req
+  const {
+    query: { id },
+    method,
+  } = req;
 
-    await dbConnect();
+  await dbConnect();
 
-    switch (method) {
-        case 'GET':
-            try {
-                const project = await Project.findById(id)
-                if (!project) return res.status(400).json({ success: false })
+  switch (method) {
+    case "GET":
+      try {
+        const project = await Project.findOne({ slug: id });
+        if (!project) return res.status(400).json({ success: false });
 
-                res.status(200).json({ success: true, data: project })
-            } catch (err) {
-                res.status(400).json({ success: false })
-            }
-            break;
-        default:
-            res.status(400).json({ success: false })
-            break;
-    }
+        res.status(200).json({ success: true, data: project });
+      } catch (err) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    default:
+      res.status(400).json({ success: false });
+      break;
+  }
 }
