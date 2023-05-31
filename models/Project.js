f10ce342dd0a71e52e95fd7bb45ab1884329058f
James McGahn <jmcgahn@gmail.com>
2023-05-31 09:59:48 -0400

    add slugify, update tour model
    - add slugify
    - update tour model for slug
    - add pre save hook to save slug

--- models/Project.js
+++ models/Project.js
@@ -1,45 +1,58 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import slugify from "slugify";
 
 const ProjectSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: [true, 'Add a Title'],
-        trim: true,
+  title: {
+    type: String,
+    required: [true, "Add a Title"],
+    trim: true,
+    unique: true,
+  },
+  slug: {
+    type: String,
+  },
+  mainPage: {
+    type: Boolean,
+  },
+  subtitle: {
+    type: String,
+    required: [true, "Add a Subtitle"],
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: [true, "Add a Description"],
+    trim: true,
+  },
+  challenges: {
+    type: String,
+    required: [true, "Add a Challenge"],
+    trim: true,
+  },
+  stack: [{ type: String, required: true }],
+  imageUrl: [
+    {
+      url: String,
+      filename: String,
     },
-    mainPage: {
-        type: Boolean,
-    },
-    subtitle: {
-        type: String,
-        required: [true, 'Add a Subtitle'],
-        trim: true,
-    },
-    description: {
-        type: String,
-        required: [true, 'Add a Description'],
-        trim: true,
-    },
-    challenges: {
-        type: String,
-        required: [true, 'Add a Challenge'],
-        trim: true,
-    },
-    stack: [{ type: String, required: true }],
-    imageUrl: [{
-        url: String,
-        filename: String,
-    }],
-    gitUrl: {
-        type: String,
-        required: [true, 'Add a Image Url'],
-        trim: true,
-    },
-    liveUrl: {
-        type: String,
-        required: [true, 'Add a Image Url'],
-        trim: true,
-    },
+  ],
+  gitUrl: {
+    type: String,
+    required: [true, "Add a Image Url"],
+    trim: true,
+  },
+  liveUrl: {
+    type: String,
+    required: [true, "Add a Image Url"],
+    trim: true,
+  },
+});
 
-})
+ProjectSchema.pre("save", function (next) {
+  this.slug = slugify(this.title, { lower: true });
+  next();
+});
 
-module.exports = mongoose.models.Project ? mongoose.models.Project : mongoose.model('Project', ProjectSchema)
+module.exports = mongoose.models.Project
+  ? mongoose.models.Project
+  : mongoose.model("Project", ProjectSchema);
