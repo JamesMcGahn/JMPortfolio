b0e8ff51ec63e4c5c93126bafbc304c5b9406ade
James McGahn <jmcgahn@gmail.com>
2023-06-01 23:14:15 -0400

    clean up layout comps

--- components/layout/PageHead.js
+++ components/layout/PageHead.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import Head from 'next/head'
+import Head from 'next/head';
 
 function PageHead({ title }) {
-    return (
-        <Head>
-            <title>{title}</title>
-            <meta property="og:title" content={title} key="title" />
-        </Head>
-    );
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta property="og:title" content={title} key="title" />
+    </Head>
+  );
 }
 
 export default PageHead;
